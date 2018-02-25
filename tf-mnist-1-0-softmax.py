5d4064dc0079495356e9a1ced99e931971d2ef6f
Johan Steunenberg <git@steunenberg.de>
2018-02-25 21:35:42 +0100

    Added first three networks

---
+++ tf-mnist-1-0-softmax.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data as mnist_data
+print("Tensorflow version " + tf.__version__)
+tf.set_random_seed(0)
+
+RUNS = 2000
+
+
+# neural network with 1 layer of 10 softmax neurons
+#
+# · · · · · · · · · ·       (input data, flattened pixels)       X [batch, 784]        # 784 = 28 * 28
+# \x/x\x/x\x/x\x/x\x/    -- fully connected layer (softmax)      W [784, 10]     b[10]
+#   · · · · · · · ·                                              Y [batch, 10]
+
+# The model is:
+#
+# Y = softmax( X * W + b)
+#              X: matrix for 100 grayscale images of 28x28 pixels, flattened (there are 100 images in a mini-batch)
+#              W: weight matrix with 784 lines and 10 columns
+#              b: bias vector with 10 dimensions
+#              +: add with broadcasting: adds the vector to each line of the matrix (numpy)
+#              softmax(matrix) applies softmax on each line
+#              softmax(line) applies an exp to each value then divides by the norm of the resulting line
+#              Y: output matrix with 100 lines and 10 columns
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+# weights W[784, 10]   784=28*28
+W = tf.Variable(tf.zeros([784, 10]))
+# biases b[10]
+b = tf.Variable(tf.zeros([10]))
+
+# flatten the images into a single line of pixels
+# -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
+XX = tf.reshape(X, [-1, 784])
+
+# The model
+Y = tf.nn.softmax(tf.matmul(XX, W) + b)
+
+# cross-entropy
+# log takes the log of each element, * multiplies the tensors element by element
+# reduce_mean will add all the components in the tensor
+# so here we end up with the total cross-entropy for all images in the batch
+cross_entropy = -tf.reduce_mean(Y_ * tf.log(Y)) * 1000.0  # normalized for batches of 100 images,
+                                                          # *10 because  "mean" included an unwanted division by 10
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# training, learning rate = 0.005
+train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+df = pd.DataFrame(columns=['train_accuracy', 'test_accuracy','train_cross_entropy', 'test_cross_entropy'])
+
+for i in range(RUNS):
+    batch_X, batch_Y = mnist.train.next_batch(100)
+    train_data = {X:batch_X, Y_: batch_Y}
+    
+    sess.run(train_step, feed_dict=train_data)
+    
+    #success?
+    a, c = sess.run([accuracy, cross_entropy], feed_dict=train_data)
+    
+    # success on test data?
+    test_data = {X : mnist.test.images, Y_ : mnist.test.labels}
+    at, ct = sess.run([accuracy, cross_entropy], feed_dict=test_data)
+    
+    df.loc[len(df)] = [a, at, c, ct]
+    
+# display results
+df.plot(subplots=True)
+plt.show()
---
+++ tf-mnist-2-0-five_layers_sigmoid.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data as mnist_data
+print("Tensorflow version " + tf.__version__)
+tf.set_random_seed(0)
+
+RUNS = 2000
+
+# neural network with 5 layers
+#
+# · · · · · · · · · ·          (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
+# \x/x\x/x\x/x\x/x\x/       -- fully connected layer (sigmoid)      W1 [784, 200]      B1[200]
+#  · · · · · · · · ·                                                Y1 [batch, 200]
+#   \x/x\x/x\x/x\x/         -- fully connected layer (sigmoid)      W2 [200, 100]      B2[100]
+#    · · · · · · ·                                                  Y2 [batch, 100]
+#     \x/x\x/x\x/           -- fully connected layer (sigmoid)      W3 [100, 60]       B3[60]
+#      · · · · ·                                                    Y3 [batch, 60]
+#       \x/x\x/             -- fully connected layer (sigmoid)      W4 [60, 30]        B4[30]
+#        · · ·                                                      Y4 [batch, 30]
+#         \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
+#          ·                                                        Y5 [batch, 10]
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+
+# five layers and their number of neurons (tha last layer has 10 softmax neurons)
+L = 200
+M = 100
+N = 60
+O = 30
+
+# Weights initialised with small random values between -0.2 and +0.2
+# When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
+W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
+B1 = tf.Variable(tf.zeros([L]))
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.zeros([M]))
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.zeros([N]))
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.zeros([O]))
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
+
+# The model
+# flatten the images into a single line of pixels
+# -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
+XX = tf.reshape(X, [-1, 784])
+# the layers
+Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
+Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+Ylogits = tf.matmul(Y4, W5) + B5
+Y = tf.nn.softmax(Ylogits)
+
+# cross-entropy loss function (= -sum(Y_i * log(Yi)) ), normalised for batches of 100  images
+# TensorFlow provides the softmax_cross_entropy_with_logits function to avoid numerical stability
+# problems with log(0) which is NaN
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
+cross_entropy = tf.reduce_mean(cross_entropy)*100
+
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+# training, learning rate = 0.003
+# training step, learning rate = 0.003
+learning_rate = 0.003
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+df = pd.DataFrame(columns=['train_accuracy', 'test_accuracy','train_cross_entropy', 'test_cross_entropy'])
+
+for i in range(RUNS):
+    batch_X, batch_Y = mnist.train.next_batch(100)
+    train_data = {X:batch_X, Y_: batch_Y}
+    
+    sess.run(train_step, feed_dict=train_data)
+    
+    #success?
+    a, c = sess.run([accuracy, cross_entropy], feed_dict=train_data)
+    
+    # success on test data?
+    test_data = {X : mnist.test.images, Y_ : mnist.test.labels}
+    at, ct = sess.run([accuracy, cross_entropy], feed_dict=test_data)
+    
+    df.loc[len(df)] = [a, at, c, ct]
+    
+# display results
+df.plot(subplots=True)
+plt.show()
